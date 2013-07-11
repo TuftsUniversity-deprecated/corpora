9c1ce31ad4a3fb10b48482cb56ac208a3a515c73
Mike Korcynski <mkorcy@gmail.com>
2013-07-11 15:18:17 -0400

    some default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,40 @@
+ALLOW_DOTS ||= /[a-zA-Z0-9_.:]+/
+
 SouthAsianDigitalLibrary::Application.routes.draw do
   root :to => "catalog#index"
 
   Blacklight.add_routes(self)
-  HydraHead.add_routes(self)
+   resources :catalog, :only => [:show, :update], :constraints => { :id => ALLOW_DOTS, :format => false }
+   Blacklight::Routes.new(self, {}).catalog
+   resources :unpublished, :only => :index
+   # This is from Blacklight::Routes#solr_document, but with the constraints added which allows periods in the id
+   resources :solr_document,  :path => 'catalog', :controller => 'catalog', :only => [:show, :update]
+   resources :downloads, :only =>[:show], :constraints => { :id => ALLOW_DOTS
+  }
+  resources :bookmarks, :path => 'catalog'
+  resources :search_history, :path => 'search_history', :only => [:index,:show]
 
-  devise_for :users
+   HydraHead.add_routes(self)
+
+   #mount HydraEditor::Engine => '/'
+   post 'records/:id/publish', to: 'records#publish', as: 'publish_record', constraints: { id: ALLOW_DOTS }
+
+   resources :records, only: [:destroy], constraints: { id: ALLOW_DOTS } do
+     member do
+       delete 'cancel'
+     end
+     resources :attachments, constraints: { id: ALLOW_DOTS }
+   end
+
+   resources :generics, only: [:edit, :update], constraints: { id: ALLOW_DOTS }
+
+   devise_for :users
+  # mount Hydra::RoleManagement::Engine => '/'
+
+#  Blacklight.add_routes(self)
+#  HydraHead.add_routes(self)
+
+  #devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
