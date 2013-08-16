856b7b42e1eb8e890ba416da137b83babe94c7df
Mike Korcynski <mkorcy@gmail.com>
2013-08-16 15:24:11 -0400

    starter code for url swapping

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815193311) do
+ActiveRecord::Schema.define(:version => 20130816190738) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -118,5 +118,13 @@ ActiveRecord::Schema.define(:version => 20130815193311) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "video_urls", :force => true do |t|
+    t.string   "pid"
+    t.string   "link"
+    t.boolean  "active"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
