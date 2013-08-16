afbc2ba7bfef202565c460ff0c291095dc43525a
Stephen McDonald <Stephen.McDonald@tufts.edu>
2013-08-16 15:39:48 -0400

    initial support for place/location data.  map appears on Places tab with clickable markers for each location.  locations in trancript are linked to map.  list of all locations can be displayed.  the map often displays wrong, text needs better formatting, no google api key is used yet.

--- app/assets/javascripts/annotate.js
+++ app/assets/javascripts/annotate.js
@@ -27,6 +27,10 @@ function initDataAndTabs()
     elements = people.concat(concepts, places);
     initTabs();
     annotateTranscript();
+
+    initMap();
+    initMarkers();
+    showMapCanvas();
 };
 
 // hold details like div name and detailed template for each of the person, concept and place
@@ -47,7 +51,7 @@ function initConfigHash()
 
         'place': {'name': 'place', 'tabId': '#tab4',
             'divId': '#placesDiv',
-        'detailTemplate': "{{name}}<br/>{{description}}<br/>{{link}}<br/><a href='javascript:showList(\"{{type}}\")'>Go To List</a>"}
+            'detailTemplate': "{{name}}<br/><div class='elementDescription'>Description: {{description}}</div><br/><div class='elementLink>'>Link: <a href='{{link}}' target='_blank'>{{link}}</a></div><br/>{{#image_link}}<div class='description-image'><img src='{{image_link}}'></div>{{/image_link}}<div class='description-link'><a href='javascript:showList(\"{{type}}\")'>Show {{type}} list</a></div>"}
     };
 };
 
@@ -88,17 +92,27 @@ function initTabsAux(type)
 
 
 // display the element corresponding to the passed name in the proper tab
-function showElement(name)
+function showElement(name, forceList)
 {
     var element = getElement(name);
     if (element == null)
         return;
     var type = element.type;
     var configHash = extraData[type];
-    var div = jQuery(configHash.divId);
-    var template = configHash.detailTemplate;
-    var text = Mustache.render(template, element);
-    div.html(text);
+    if (type == "place" && forceList != true)
+    {
+        highlightMapByName(name);
+        showMapCanvas();
+    }
+    else
+    {
+        var div = jQuery(configHash.divId);
+        var template = configHash.detailTemplate;
+        var text = Mustache.render(template, element);
+        div.html(text);
+        if (type == "place")
+            showLocationIndex();
+    }
     jQuery(configHash.tabId).click();
 };
 
@@ -164,6 +178,116 @@ function createRegex(records)
     regex += ")";
     return regex;
 };
+
+// below is code to deal with the map
+// should it be in this file?
+
+var map = null;
+
+function initMap()
+{
+    var mapOptions = {
+        center: new google.maps.LatLng(42., -71.),
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        zoomControl: true,
+        panControl: true,
+        scaleControl: true
+    };
+    map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+
+};
+
+function showLocationIndex()
+{
+    jQuery("#mapParent").hide();
+    jQuery("#placesParentDiv").show();
+};
+
+function showMapCanvas()
+{
+    jQuery("#placesParentDiv").hide();
+    jQuery("#mapParent").show();
+    google.maps.event.trigger(map, 'resize');
+};
+
+
+function initMarkers()
+{
+    var places = extraData.place.data;
+
+    for (var i = 0 ; i < places.length ; i++)
+    {
+        var place = places[i];
+        var latitude = place.latitutde;
+        var longitude = place.longitude;
+        var name = place.name;
+        var position = new google.maps.LatLng(latitude, longitude);
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: name});
+        addListener(marker);
+    }
+
+};
+
+function markerClickHandler(marker)
+{
+    var name = marker.getTitle();
+    highlightMapByName(name);
+}
+
+function highlightMapByName(name)
+{
+    var element = getElement(name);
+    if (name == null)
+      return;
+    var template = "Name: {{name}}<br/>Description: {{description}}<br><a href='javascript:showElement(\"{{name}}\", true)'>More Info</a>";
+    var text = Mustache.render(template, element);
+    infoWindow = new google.maps.InfoWindow();
+    infoWindow.setContent(text);
+    infoWindow.setPosition(new google.maps.LatLng(element.latitutde, element.longitude));
+    map.setZoom(locationTypeToZoomLevel(element.location_type));
+    infoWindow.open(map);
+
+}
+
+function addListener(marker)
+{
+    google.maps.event.addListener(marker, 'click', function (event) {markerClickHandler(marker)});
+}
+
+function locationTypeToZoomLevel(passedLocationType)
+{
+    var zoomLevel = 7;
+    if ((passedLocationType == "continent") || (passedLocationType == "ocean"))
+        zoomLevel = 4;
+    else if ((passedLocationType == "country"))
+        zoomLevel = 5;
+    else if ((passedLocationType == "university") || (passedLocationType == "business") || (passedLocationType == "college/university")
+|| (passedLocationType == "library")
+    || (passedLocationType == "military base")
+|| (passedLocationType == "private school"))
+    zoomLevel = 15;
+else if ((passedLocationType == "residence")|| (passedLocationType == "building/structure")
+    || (passedLocationType == "neighborhood") || (passedLocationType == "railway station")
+    || (passedLocationType == "church")
+    || (passedLocationType == "institution") || (passedLocationType == "hostel") || (passedLocationType == "hospital")
+    || (passedLocationType == "bazar/market")
+    || (passedLocationType == "museum") || (passedLocationType == "night club") || (passedLocationType == "holy site")
+    || (passedLocationType == "government building") || (passedLocationType == "road") || (passedLocationType == "recreation"))
+    zoomLevel = 17;
+else if ((passedLocationType == "city/town") || (passedLocationType == "capital") || (passedLocationType == "lake"))
+    zoomLevel = 11;
+else if ((passedLocationType == "region") || (passedLocationType == "state") || (passedLocationType == "island")
+    || (passedLocationType == "territory")
+    || (passedLocationType == "district"))
+    zoomLevel = 6;
+    return zoomLevel;
+}
+
+
 
 
 
