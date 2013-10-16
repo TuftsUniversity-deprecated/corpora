af27fc17b444fb64bf0ea37818bad2d5c8682588
Mike Korcynski <mkorcy@gmail.com>
2013-10-16 14:03:42 -0400

    annotate ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include Hydra::Ability
 
   def custom_permissions
+
     if current_user.admin?
       can [:create, :show, :add_user, :remove_user, :index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], Role
       #Rails Admin Verbs
@@ -9,13 +10,14 @@ class Ability
       #:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit
        #:show,:edit :destroy :history,:show_in_app,
       can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], Concept
+      can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], Annotation
       can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], Person
       can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], VideoUrl
       can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], Location
       can [:index, :new, :export, :history, :destroy, :show, :show_in_app, :edit], User
 
       #not actually doing anything with the following in this app
-      #can [:create, :edit, :update, :publish, :destroy], ActiveFedora::Base
+#      can [:create, :edit, :update, :publish, :destroy], ActiveFedora::Base
     end
   end
 
@@ -24,6 +26,10 @@ class Ability
       if user.has_role? :admin
         can :access, :rails_admin   # grant access to rails_admin
         can :dashboard              # grant access to the dashboard
+      end
+
+      if current_user.has_role? :annotator
+        can [:annotate], CatalogController
       end
 
       if user.has_role? :student
