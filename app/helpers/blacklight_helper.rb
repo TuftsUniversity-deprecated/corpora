d503ef6b91327a6378b601cdff6a088f1cc67518
Mike Korcynski <mkorcy@gmail.com>
2013-10-22 16:30:05 -0400

    fixes problem with document partial name, should be extracted into tuftsification i think

---
+++ app/helpers/blacklight_helper.rb
@@ -0,0 +1,10 @@
+module BlacklightHelper
+  include Hydra::BlacklightHelperBehavior
+
+  # currently only used by the render_document_partial helper method (below)
+  def document_partial_name(document)
+logger.warn "TEXT"
+       return if document[CatalogController.blacklight_config.show.display_type].nil?
+       ModelNameHelper.map_model_name(document[CatalogController.blacklight_config.show.display_type].first).gsub(/^[^\/]+\/[^:]+:/, "").underscore.pluralize
+  end
+end
