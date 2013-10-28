48414cddd4abf8d8c73eb573f70cf6e2269d7e56
Mike Korcynski <mkorcy@gmail.com>
2013-10-28 17:12:59 -0400

    change app name to corpora

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -3,8 +3,14 @@ module BlacklightHelper
 
   # currently only used by the render_document_partial helper method (below)
   def document_partial_name(document)
-logger.warn "TEXT"
+      #logger.warn "TEXT"
        return if document[CatalogController.blacklight_config.show.display_type].nil?
        ModelNameHelper.map_model_name(document[CatalogController.blacklight_config.show.display_type].first).gsub(/^[^\/]+\/[^:]+:/, "").underscore.pluralize
   end
+
+
+    def application_name
+      "Corpora"
+    end
+
 end
