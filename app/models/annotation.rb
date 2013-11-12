01d11de4385da4fe95996441487b10227e02b713
Mike Korcynski <mkorcy@gmail.com>
2013-11-12 15:32:15 -0500

    reindexing on annotation crud

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,4 +1,17 @@
+
 class Annotation < ActiveRecord::Base
   attr_accessible :json, :text, :pid, :term, :term_type, :utterance
   serialize :json, JSON
+  after_save :index_object
+  after_destroy :index_object
+
+  def index_object
+    reindex_object self.pid
+  end
+
+  def reindex_object pid
+    @document_fedora = TuftsBase.find(pid, :cast=>true)
+    @document_fedora.update_index
+  end
+  handle_asynchronously :reindex_object
 end
