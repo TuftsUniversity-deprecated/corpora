1c6deb9d7b5e25ef30aa849607f9b09f149e1242
Mike Korcynski <mkorcy@gmail.com>
2013-11-12 15:32:32 -0500

    reindexing on crud

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,3 +1,8 @@
+require 'delayed_indexing'
 class Concept < ActiveRecord::Base
+  include DelayedIndexing
   attr_accessible :name, :alternative_names, :description, :link, :image_link
+  after_save :after_save
+  after_destroy :after_save
+
 end
