805bb550d8fb388399b60e8c6c554511103ce18e
Mike Korcynski <mkorcy@gmail.com>
2013-11-12 15:34:25 -0500

    add delayed indexing

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
+require 'delayed_indexing'
 class Location < ActiveRecord::Base
+  include DelayedIndexing
   attr_accessible :admin01, :admin02, :description, :external_feature_id, :historical_name, :latitutde, :link, :location_type, :longitude, :modern_location, :name, :town, :variable_names
+  after_save :after_save
+  after_destroy :after_save
 end
