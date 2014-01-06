0cf716a13da48d07c9f04bee080da28fd5ef98a1
Mike Korcynski <mkorcy@gmail.com>
2014-01-06 15:17:13 -0500

    enabling of rails asset pipeline, brings in line with tuftsification changes for TDL

--- app/assets/javascripts/tuftsification.js
+++ app/assets/javascripts/tuftsification.js
@@ -10,13 +10,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//
-// Required by Blacklight
-//= require blacklight/blacklight
-//= require bootstrap
-//= require concepts
-//= require typeahead
-
-
+//= require 'tuftsification-hydra'
